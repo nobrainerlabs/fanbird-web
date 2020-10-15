c74f31d1201985c8fd89bc0c6ce9b641920e5a03
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-15 23:44:37 +0300

    layout added

--- src/components/layouts/mission.tsx
+++ src/components/layouts/mission.tsx
@@ -1,6 +1,23 @@
 const Layout = ({ children }) => (
   <>
-    {children}
+      <div className="body h-screen">             
+            <div className="main-div pt-24 flex justify-center items-center flex-col">
+                <div className="logo text-center mb-4">
+                    <img className="logo-image rounded-full w-full h-full" src="../images/logo.png" />
+                </div>
+
+                <div className="header-and-content text-center mb-4">
+                    <h3 className="text-3xl font-semibold">Coolhaus</h3>
+                    <p className="text-base">Awesome Ice Cream</p>     
+                </div>
+            </div>
+
+            <div>
+              {children}
+            </div>
+
+        </div>
+
     <style jsx global>{`
       *,
       *::before,
