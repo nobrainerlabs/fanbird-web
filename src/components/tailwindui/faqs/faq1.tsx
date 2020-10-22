092878a7beacbfc57b1f9adb44970f6ca7ad479d
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-23 01:09:34 +0300

    solved

--- src/components/tailwindui/faqs/faq1.tsx
+++ src/components/tailwindui/faqs/faq1.tsx
@@ -1,28 +1,28 @@
 const Component = ({}) => (
     <>
     
-<div class="bg-gray-50">
-  <div class="max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-    <div class="max-w-3xl mx-auto">
-      <div class="mt-6 border-t-2 border-gray-200 pt-6">
+<div className="bg-gray-50">
+  <div className="max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+    <div className="max-w-3xl mx-auto">
+      <div className="mt-6 border-t-2 border-gray-200 pt-6">
         <dl>
         <div>
-            <dt class="text-lg leading-7">
+            <dt className="text-lg leading-7">
             
-              <button class="text-left w-full flex justify-between items-start text-gray-400 focus:outline-none focus:text-gray-900">
-                <span class="font-medium text-gray-900">
+              <button className="text-left w-full flex justify-between items-start text-gray-400 focus:outline-none focus:text-gray-900">
+                <span className="font-medium text-gray-900">
                   What's the best thing about Switzerland?
                 </span>
-                <span class="ml-6 h-7 flex items-center">
+                <span className="ml-6 h-7 flex items-center">
                   
-                  <svg class="-rotate-180 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg className="-rotate-180 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                   </svg>
                 </span>
               </button>
             </dt>
-            <dd class="mt-2 pr-12">
-              <p class="text-base leading-6 text-gray-500">
+            <dd className="mt-2 pr-12">
+              <p className="text-base leading-6 text-gray-500">
                 I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.
               </p>
             </dd>
