3381e39c0764897e6b0d259534764435100ee4b7
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-23 01:14:14 +0300

    solvedd

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -4,12 +4,7 @@ import Faq1  from "../components/tailwindui/faqs/faq1";
 
 const Page = () => (
 <Layout>
-    <Header
-        headerPage="FAQ"
-        headerTitle="Frequently Asked Questions"
-        headerDescription=""
-    >
-    </Header>
+    <Header></Header>
     <Faq1></Faq1>
     
 </Layout>
